a73880052faf5f5cf678ce1db5987f7151335d95
LizaKoval <liza.koval.01@bk.ru>
2022-03-09 17:54:08 +0300

    I've found the time of the longest ride and changed repositories name

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import csv
-# import datetime # буду использовать для считывания формата даты
+import datetime # used for dates
 
 FILENAME = "202003-capitalbikeshare-tripdata.csv"
+
+def max_time_of_trip(arr): # finding the longest ride in the file
+   max_ = arr[0].trip_duration
+   k=0
+   for i in range(len(arr) - 1):
+      if arr[i].trip_duration > max_:
+         max_=arr[i].trip_duration
+   print(f'Самая продолжительная поездка: {max_} ')
+   return max_
 
 class Trip:
 
@@ -15,44 +24,56 @@ class Trip:
    bike_number = ''
    member_type = ''
 
-   trips = [] # массив объектов
-   bikes=0 # счётчик байков
+   trips = [] # special list for Trip's objects
+   bikes=0 # bikes counter
 
-   def __init__(self): # конструктор
-     pass
+   def __init__(self): # builder
+      pass
+  # @staticmethod
+  # def trips_number():
+    #  trips_number = len(Trip.trips)
+    #  return trips_number
 
-   @staticmethod
-   def trips_number():
-      trips_number = len(Trip.trips)
-      return trips_number
-
-   @staticmethod
-   def bikes_number():
-      for i in range(len(Trip.trips) - 1):
-         if (Trip.trips[i].bike_number != Trip.trips[i + 1].bike_number):
-            Trip.bikes += 1
-      return Trip.bikes
+   # @staticmethod
+   #def bikes_number():
+   #   for i in range(len(Trip.trips) - 1):
+      #   if (Trip.trips[i].bike_number != Trip.trips[i + 1].bike_number):
+        #    Trip.bikes += 1
+     # return Trip.bikes
 
 with open(FILENAME, "r", newline="") as csvfile:
    reader = csv.DictReader(csvfile, delimiter=',')
-   for row in reader: # пытаюсь прочитать данные из файла в объект класса Trip
+   for row in reader: # reading data from file into Trip's objects
       t = Trip()
       t.duration = int(row.get('Duration'))
-      t.start_date = row.get('Start date')
-      t.end_date = row.get('End date')
+      start_date_str = row.get('Start date')
+      t.start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d %H:%M:%S")
+      t.end_date = datetime.datetime.strptime(row.get('End date'), "%Y-%m-%d %H:%M:%S")
       t.start_st_number = int(row.get('Start station number'))
       t.start_station = row.get('Start station')
       t.end_station_number = int(row.get('End station number'))
       t.end_station = row.get('End station')
       t.bike_number = row.get('Bike number')
       t.member_type = row.get('Member type')
-      print(t)                                     # УДАЛИТЬ ПОТОМ!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      t.trip_duration = t.end_date - t.start_date
+      print(t)                                     # DELETE LATER !!!!!!!!!!!!!!!!!!!!!!!!!!!!
       Trip.trips.append(t)
 
+trips_number = len(Trip.trips) # total amount of trips: this function
+                              # has been already realised as staticmethod for class Trip in line 32
+                              # but if i understood clear my task was to clean Trip class from any calculations ...so I did it
+
+for i in range(len(Trip.trips) - 1): # total amount of bikes used
+   if (Trip.trips[i].bike_number != Trip.trips[i + 1].bike_number):
+      Trip.bikes += 1
+
+longest_trip=max_time_of_trip(Trip.trips)
+
 with open('general-stats.csv', "a", newline="") as csvfile:
-     writer = csv.writer(csvfile, delimiter=' ') # delimiter - это разделитель, по умолчанию была бы запятая, но она не нужна
-     writer.writerow(['Количество поездок = ', Trip.trips_number()]) #  3 ЗАДАНИЕ_1 ЧАСТЬ
-     writer.writerow(['Количество велосипедов за первый квартал = ', Trip.bikes_number()]) #  3 ЗАДАНИЕ_3 ЧАСТЬ
+    writer = csv.writer(csvfile, delimiter=' ')
+    writer.writerow(['Количество поездок = ', trips_number]) #  3 task part a
+    writer.writerow(["Время самой продолжительной поездки = ", longest_trip])#  3 task part b
+    writer.writerow(['Количество велосипедов за первый квартал = ', Trip.bikes]) #  3 task part c
 
 
 
