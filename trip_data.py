335977a19fba05e428ceb249950d6b808fbdcc67
LizaKoval <liza.koval.01@bk.ru>
2022-03-11 12:12:22 +0300

    I've found the time of the longest ride and changed repositories name

--- trip_data.py
+++ trip_data.py
@@ -3,13 +3,7 @@ import datetime # used for dates
 import services # users module with all necessary
                 # instruments(methods)
 
-class FileName:
-   filename = ''
-
-   def __init__(self, filename):
-      self.filename = filename
-
-class TripData(FileName): # main class with data read from csv file
+class TripData: # main class with data read from csv file
 
    duration = ''
    start_date = ''
@@ -28,23 +22,30 @@ class TripData(FileName): # main class with data read from csv file
       pass
 
 
-with open(FILENAME, "r", newline="") as csvfile:
-   reader = csv.DictReader(csvfile, delimiter=',')
-   for row in reader: # reading data from file into Trip's objects
-      t = TripData()
-      t.duration = int(row.get('Duration'))
-      start_date_str = row.get('Start date')
-      t.start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d %H:%M:%S")
-      t.end_date = datetime.datetime.strptime(row.get('End date'), "%Y-%m-%d %H:%M:%S")
-      t.start_st_number = int(row.get('Start station number'))
-      t.start_station = row.get('Start station')
-      t.end_station_number = int(row.get('End station number'))
-      t.end_station = row.get('End station')
-      t.bike_number = row.get('Bike number')
-      t.member_type = row.get('Member type')
-      t.trip_duration = t.end_date - t.start_date
-      print(t)                                     # DELETE LATER !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      TripData.trips.append(t)
+class FileReader:
+   filename = ''
+
+   def __init__(self, filename):
+      self.filename = filename
+
+   def reader(self):
+      with open(self.filename, "r", newline="") as csvfile:
+         reader = csv.DictReader(csvfile, delimiter=',')
+         for row in reader:  # reading data from file into Trip's objects
+            t = TripData()
+            t.duration = int(row.get('Duration'))
+            start_date_str = row.get('Start date')
+            t.start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d %H:%M:%S")
+            t.end_date = datetime.datetime.strptime(row.get('End date'), "%Y-%m-%d %H:%M:%S")
+            t.start_st_number = int(row.get('Start station number'))
+            t.start_station = row.get('Start station')
+            t.end_station_number = int(row.get('End station number'))
+            t.end_station = row.get('End station')
+            t.bike_number = row.get('Bike number')
+            t.member_type = row.get('Member type')
+            t.trip_duration = t.end_date - t.start_date
+            print(t)  # DELETE LATER !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            TripData.trips.append(t)
 
 #class ResultSaver(TripData): # class for saving processed data in final files
 
